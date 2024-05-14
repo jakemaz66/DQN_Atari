f78753ef3a24f02e620f781b2b820b6bdc90d09d
jakem <mazurkiewiczj@duq.edu>
2024-05-14 07:42:03 -0400

    Changed Wrappers

--- DQN_Atari/preprocess_frames.py
+++ DQN_Atari/preprocess_frames.py
@@ -1,26 +1,159 @@
-import torch
-import numpy as np
 import cv2
+import gym
+import gym.spaces
+import numpy as np
+import collections
+from typing import Dict, SupportsFloat
+
+import gymnasium as gym
+import numpy as np
+from gymnasium import spaces
+
+from stable_baselines3.common.type_aliases import AtariResetReturn, AtariStepReturn
+
+try:
+    import cv2
+
+    cv2.ocl.setUseOpenCL(False)
+except ImportError:
+    cv2 = None  # type: ignore[assignment]
+
+class FireResetEnv(gym.Wrapper[np.ndarray, int, np.ndarray, int]):
+    """
+    Take action on reset for environments that are fixed until firing.
+
+    :param env: Environment to wrap
+    """
+
+    def __init__(self, env: gym.Env) -> None:
+        super().__init__(env)
+        assert env.unwrapped.get_action_meanings()[1] == "FIRE"  # type: ignore[attr-defined]
+        assert len(env.unwrapped.get_action_meanings()) >= 3  # type: ignore[attr-defined]
+
+    def reset(self, **kwargs) -> AtariResetReturn:
+        self.env.reset(**kwargs)
+        obs, _, terminated, truncated, _ = self.env.step(1)
+        if terminated or truncated:
+            self.env.reset(**kwargs)
+        obs, _, terminated, truncated, _ = self.env.step(2)
+        if terminated or truncated:
+            self.env.reset(**kwargs)
+        return obs, {}
+
+class MaxAndSkipEnv(gym.Wrapper[np.ndarray, int, np.ndarray, int]):
+    """
+    Return only every ``skip``-th frame (frameskipping)
+    and return the max between the two last frames.
+
+    :param env: Environment to wrap
+    :param skip: Number of ``skip``-th frame
+        The same action will be taken ``skip`` times.
+    """
+
+    def __init__(self, env: gym.Env, skip: int = 4) -> None:
+        super().__init__(env)
+        # most recent raw observations (for max pooling across time steps)
+        assert env.observation_space.dtype is not None, "No dtype specified for the observation space"
+        assert env.observation_space.shape is not None, "No shape defined for the observation space"
+        self._obs_buffer = np.zeros((2, *env.observation_space.shape), dtype=env.observation_space.dtype)
+        self._skip = skip
+
+    def step(self, action: int) -> AtariStepReturn:
+        """
+        Step the environment with the given action
+        Repeat action, sum reward, and max over last observations.
+
+        :param action: the action
+        :return: observation, reward, terminated, truncated, information
+        """
+        total_reward = 0.0
+        terminated = truncated = False
+        for i in range(self._skip):
+            obs, reward, terminated, truncated, info = self.env.step(action)
+            done = terminated or truncated
+            if i == self._skip - 2:
+                self._obs_buffer[0] = obs
+            if i == self._skip - 1:
+                self._obs_buffer[1] = obs
+            total_reward += float(reward)
+            if done:
+                break
+        # Note that the observation on the done=True frame
+        # doesn't matter
+        max_frame = self._obs_buffer.max(axis=0)
+
+        return max_frame, total_reward, terminated, truncated, info
 
 
-def preprocess_frames(frame):
-    """This function preprocesses Atari Pong Frames"""
-    #Cropping
-    frame[30:-12, 5:-4]
+class ProcessFrame84(gym.ObservationWrapper):
+    def __init__(self, env=None):
+        super(ProcessFrame84, self).__init__(env)
+        self.observation_space = gym.spaces.Box(
+            low=0, high=255, shape=(84, 84, 1), dtype=np.uint8)
 
-    #Grayscale
-    frame = np.average(frame, axis=2)
+    def observation(self, obs):
+        return ProcessFrame84.process(obs)
 
-    frame = cv2.resize(frame, (84, 84), interpolation=cv2.INTER_NEAREST)
+    @staticmethod
+    def process(frame):
+        if frame.size == 210 * 160 * 3:
+            img = np.reshape(frame, [210, 160, 3]).astype(
+                np.float32)
+        elif frame.size == 250 * 160 * 3:
+            img = np.reshape(frame, [250, 160, 3]).astype(
+                np.float32)
+        else:
+            assert False, "Unknown resolution."
+        img = img[:, :, 0] * 0.299 + img[:, :, 1] * 0.587 + \
+              img[:, :, 2] * 0.114
+        resized_screen = cv2.resize(
+            img, (84, 110), interpolation=cv2.INTER_AREA)
+        x_t = resized_screen[18:102, :]
+        x_t = np.reshape(x_t, [84, 84, 1])
+        return x_t.astype(np.uint8)
 
-    return torch.tensor(frame)
+
+class ImageToPyTorch(gym.ObservationWrapper):
+    def __init__(self, env):
+        super(ImageToPyTorch, self).__init__(env)
+        old_shape = self.observation_space.shape
+        new_shape = (old_shape[-1], old_shape[0], old_shape[1])
+        self.observation_space = gym.spaces.Box(
+            low=0.0, high=1.0, shape=new_shape, dtype=np.float32)
+
+    def observation(self, observation):
+        return np.moveaxis(observation, 2, 0)
 
 
-def stack_frames(state,frames):
-    """This function stacks the four most recent frames"""
+class ScaledFloatFrame(gym.ObservationWrapper):
+    def observation(self, obs):
+        return np.array(obs).astype(np.float32) / 255.0
 
-    frames.append(state)
-    return frames
 
-if __name__ == '__main__':
-    preprocess_frames(np.array([[[3]]]))
+class BufferWrapper(gym.ObservationWrapper):
+    def __init__(self, env, n_steps, dtype=np.float32):
+        super(BufferWrapper, self).__init__(env)
+        self.dtype = dtype
+        old_space = env.observation_space
+        self.observation_space = gym.spaces.Box(old_space.low.repeat(n_steps, axis=0),
+                                                old_space.high.repeat(n_steps, axis=0), dtype=dtype)
+
+    def reset(self, **kwargs):
+        self.buffer = np.zeros_like(self.observation_space.low, dtype=self.dtype)
+        obs, info = self.env.reset(**kwargs)
+        return self.observation(obs), info
+
+    def observation(self, observation):
+        self.buffer[:-1] = self.buffer[1:]
+        self.buffer[-1] = observation
+        return self.buffer
+
+
+def make_env(env_name):
+    env = gym.make(env_name)
+    env = MaxAndSkipEnv(env)
+    env = FireResetEnv(env)
+    env = ProcessFrame84(env)
+    env = ImageToPyTorch(env)
+    env = BufferWrapper(env, 4)
+    return ScaledFloatFrame(env)
--- DQN_Atari/training_loop_atari.py
+++ DQN_Atari/training_loop_atari.py
@@ -1,54 +1,31 @@
 import config, function_approximators, memory_replay
-import gym
+import gymnasium as gym
 import torch
 from torch import nn
 import numpy as np
 import torch.optim as optim
 from collections import deque
-from preprocess_frames import preprocess_frames, stack_frames
+from wrapper import make_env
 import matplotlib.pyplot as plt
+import math
 
 
-def initialize_game(env_name, optimizer, memory_replay_length):
-    """This function initializes the gym environment, the function approximation networks, the optimizer,
-       and the replay memory from which to sample transitions
-    """
-    #Setting up the gym environment
-    env = gym.make(env_name)
-
-    #observation_space is a Box() class of coordinate positions, access dimensions with _shape
-    action_space = env.action_space.n
-
-    #Defining Q function approximators with inputs of 4 frames by size 84x84
-    policy_network = function_approximators.AtariFunctionApproximator((4,84,84), action_space)
-    target_network = function_approximators.AtariFunctionApproximator((4,84,84), action_space)
-
-    #Setting the parameters of the target network equal to the policy network
-    target_network.load_state_dict(policy_network.state_dict())
-
-    #Initializing optimizer and prioritized replay memory
-    optm = optimizer(policy_network.parameters(), lr=config.LR, amsgrad=True)
-    replay_memory = memory_replay.PrioritizedMemoryReplay(max_len=memory_replay_length)
-
-    return env, optm, replay_memory, policy_network, target_network
-
-
-def update_params(loss_fn = nn.SmoothL1Loss()):
+def update_params():
     """This function updates the parameters of the policy network"""
 
     #If the agent has not accumulated enough experience yet
-    if replay_memory.length() < config.BATCHSIZE:
+    if len(replay_memory)< config.BATCHSIZE:
         return
     
     #Sampling from the replay memory, samples is a batch of named tuples
     #Weights are from prioritized replay importance sampling
-    samples, indices, weights = replay_memory.sample(config.BATCHSIZE)
+    samples = replay_memory.sample(config.BATCHSIZE)
 
     #Gathering all the states, actions, and rewards from sample
     batch = memory_replay.EnvStep(*zip(*samples))
 
     #Creating new tensors of concatenated values along the dimensions of the named tuple
-    state_batch = torch.cat(batch.state).reshape(config.BATCHSIZE, 4, 84, 84).to(torch.float32)
+    state_batch = torch.stack(batch.state, dim=0).to(torch.float32)
     action_batch = torch.cat(batch.action)
     reward_batch = torch.cat(batch.reward)
 
@@ -64,7 +41,7 @@ def update_params(loss_fn = nn.SmoothL1Loss()):
     
     #Get all the non_final states states for time step t + 1
     nf_states = [s for s in batch.next_state if s is not None]
-    non_final_states = torch.cat(nf_states).reshape(len(nf_states), 4, 84, 84).to(torch.float32)
+    non_final_states =  torch.stack(nf_states, dim=0).to(torch.float32)
     with torch.no_grad():
         #Getting the predicted Q values from the target network, taking the max Q-value
         next_step_value_function[non_final_mask] = target_network(non_final_states).max(1).values
@@ -74,26 +51,20 @@ def update_params(loss_fn = nn.SmoothL1Loss()):
 
     #The loss is the initial q value estimate subtracted by the estimate after one time step (after we gain more information)
     #q_value_function is the Q values of the actions we did take, next_q_val is the immediate reward + the max Q val of the next state
-    loss = loss_fn(q_value_function, next_q_value_function.unsqueeze(1))
-    loss_ret = (loss.detach()*weights) + 1e-5
+    criterion = nn.SmoothL1Loss()
+    loss = criterion(q_value_function.squeeze(0), next_q_value_function)
 
-    #clipping the loss to between (0,1) for stability
-    loss = torch.clip(loss, min=-1, max=1)
-
-    optm.zero_grad()
+    # Optimize the model
+    optimizer.zero_grad()
     loss.backward()
-
-    #Updating transition priorities
-    replay_memory.update_priorities(indices, loss_ret)
-
-    #We clip the gradient in order to protect against exploding gradients
+    # In-place gradient clipping
     torch.nn.utils.clip_grad_value_(policy_network.parameters(), 100)
-    optm.step()
+    optimizer.step()
 
 
 #Main training loop
 if __name__ == '__main__':
-    env, optm, replay_memory, policy_network, target_network = initialize_game("Pong-v0", optim.AdamW, memory_replay_length=10_000)
+
 
     """Pong Actions:
         0: Noop
@@ -104,16 +75,28 @@ if __name__ == '__main__':
         5: Left Fire
 
     """
+    #Setting up the gym environment
+    env = make_env("PongNoFrameskip-v4")
 
+    #observation_space is a Box() class of coordinate positions, access dimensions with _shape
+    action_space = env.action_space.n
+
+    #Defining Q function approximators with inputs of 4 frames by size 84x84
+    policy_network = function_approximators.AtariFunctionApproximator((4,84,84), action_space)
+    target_network = function_approximators.AtariFunctionApproximator((4,84,84), action_space)
+
+    #Setting the parameters of the target network equal to the policy network
+    target_network.load_state_dict(policy_network.state_dict())
+
+    optimizer = optim.AdamW(policy_network.parameters(), lr=config.LR, amsgrad=True)
+
+    #Initializing optimizer and prioritized replay memory
+    replay_memory = memory_replay.ReplayMemory(capacity=100_000)
     epsilon = 1
 
-    #State is an RGB image
-    state, _ = env.reset()
-    state = preprocess_frames(state)
-    init = False
-
-    #Stack the previous 4 frames as a new state
-    frames_queue = deque([], maxlen=4)
+    #State is an image
+    state, info = env.reset()
+    state = torch.tensor(state)
 
     n_episode = 0
     episode_reward = 0
@@ -123,45 +106,31 @@ if __name__ == '__main__':
     score = deque(maxlen=100)
 
     for i in range(config.NUMFRAMES):
-
-            #Adding dummy frames to stack of episodes at beginning of each episode
-            if init == False:
-                for i in range(4):
-                    stack_frames(state, frames=frames_queue)
-                init=True
-
-            frames_tuple = tuple(tensor for tensor in frames_queue)
-            stacked_frames = torch.stack(frames_tuple).reshape((4,84,84))
-            state = stacked_frames
-             
             with torch.no_grad():
+                if state.shape != torch.Size([4,84,84]):
+                    state = torch.tensor(state).permute(2,0, 1)
+
                 new_epslion, action = policy_network.act(state, epsilon, env)
+                eps_threshold = config.EPS_END + (config.EPS_START - config.EPS_END) * math.exp(-1. * i / config.EPS_DECAY)
                 epsilon = new_epslion
 
             observation, reward, terminated, truncated, info = env.step(action.item())
 
-            #Appending new preprocessed observation frame to queue
-            frames_queue.append(preprocess_frames(observation))
-
-            #Retrieving new state from updated frame_queue
-            frames_tuple = tuple(tensor for tensor in frames_queue)
-            next_state = torch.stack(frames_tuple).reshape((4,84,84))
-
-            reward = torch.tensor([reward])
-            done = terminated or truncated
-            episode_reward += reward
+            observation = torch.tensor(observation)
             
-            #If episode is about to terminate, do not append a next state
-            if terminated:
-                next_state = None
+            reward = torch.tensor([reward])
+            
+            replay_memory.push(state, action, observation, reward)
 
-            replay_memory.add_episode(state, action, next_state, reward)
+            done = terminated or truncated
+            episode_reward += reward    
 
             #Replace the current state with the next observation from the environment
-            state = next_state
+            state = observation
 
             #update the policy network parameters
             update_params()
+
 
             #update the parameters of the target network every 10,000 frames
             if i % 10_000 == 0:
@@ -172,22 +141,18 @@ if __name__ == '__main__':
 
             #Saving model every 100,000 frames
             if i % 100_000 == 0:
-                weights_path = f'DQN_Atari/saved_models/model{i}.pth'
+                weights_path = f'DQN_Atari/saved_models/model_{i}.pth'
 
                 # Save the model's state_dict (weights and biases)
                 torch.save(target_network.state_dict(), weights_path)
                 
             #If episode is over, break out of loop and iterate episode
             if done:
-                #Decaying epsilon for lessened exploration
 
                 state, info = env.reset()
-                state = preprocess_frames(state)
-
-                init=False
+                state = torch.tensor(state)
                 n_episode += 1
                 score.append(episode_reward.item())
-                next_state = None
 
                 print(f'Episode {n_episode} is complete')
                 print(f'Reward at this episode: {episode_reward}')
